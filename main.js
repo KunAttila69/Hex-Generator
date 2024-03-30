b7cafca9b72a70ffd8fad36ff33d836acddd242d
KunAttila69 <klun.attila0508@gmail.com>
2024-03-30 20:14:51 +0100

    Design and input changes

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const UpdateValue = (e,val) => {
-    document.querySelector(`#${val}`).innerHTML = e.value
-    color.style.backgroundColor = `rgb(${sliRed.value},${sliGreen.value},${sliBlue.value})`
+
+    if(e.type === "number"){ 
+        e.value = e.value > 255 ? 255 : e.value < 0 || e.value == "" ? 0 : e.value
+    }
+
+    document.querySelector(`#${val}`).value = e.value
+    screen.style.backgroundColor = `rgb(${sliRed.value},${sliGreen.value},${sliBlue.value})`
 }
+
